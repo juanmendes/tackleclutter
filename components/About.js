fef2ed4c7f75c867f0fca5b0f78dddabaa471f98
Juan Mendes <jmendes@vmware.com>
2016-10-03 00:03:45 -0400

    New about page text

--- components/About.js
+++ components/About.js
@@ -1,60 +1,86 @@
-import React from 'react'
-import Layout from './layout/ContentAndImageOnRight'
+import React from 'react';
+import NavLink from './NavLink';
 export default function () {
-    return <Layout>
-        <div>
-            If you are reading this page, it's because you are looking for some kind of help to organize a space in
-            your house.
-            I’m so glad you are taking this step to take control of your home and finally tackle the clutter around.
-            You can do it, and I can help you! I guarantee you that with some effort, work and guidance you will
-            declutter
-            your home and make the space useful again for yourself and your family.
+    return (
+        <div className="container about-page text-justify">
+            <p>
+                If you are reading this page, you are likely looking for some kind of help to organize a space in
+                your
+                house. I’m so glad you are taking this step to organize your home and spend more of your time
+                enjoying the
+                important things in life. You can do it, and I can help you! I guarantee you that with some effort,
+                work and
+                guidance you will declutter your home and make your space useful again for you and your family. </p>
 
             <h3>A little about me</h3>
 
-            <p>I’ve always been organized from a very young age. I enjoyed organizing my dolls, my desk,
-                my closet and making my room look nice. That’s where I spent most of my time studying and hanging
-                out with my
-                friends so I liked having my room presentable. It was important to me. I can clearly remember myself
+            <p>I’ve always been organized from a very young age. I enjoyed organizing my dolls, my desk, my closet
+                and
+                making my room look nice. That’s where I spent most of my time studying and hanging out with my
+                friends
+                so I liked having my room presentable. It was important to me. I can clearly remember myself
                 organizing
-                my pink wardrobe closet (actually my whole room was PINK!). It had three tall doors, but I could
-                only use two of
-                them to keep my clothes and shoes, the other one my mom would keep our extra bed comforters.
+                my pink wardrobe closet (actually my whole room was PINK!). It had three tall doors but I could
+                only
+                use two of them to keep my clothes and shoes, the other one my mom would keep our extra bed
+                comforters.
                 I would remove everything from my shelves and hangers and declutter by deciding what I would keep,
-                pass along
-                to my younger cousins or discard. I don’t remember my mom ever asking me to organize my room or
-                closet. 
-                I was always a step ahead of her (haha what a good daughter huh?!) I’ve always been a planner.
-                I would get back from school and do all my homework or projects right away. I hated procrastination.
-                It would make me anxious and nervous if I knew I had something undone for school. I had to get it
-                done!
-            </p>
+                pass
+                along to my younger cousins or discard. I don’t remember my mom ever asking me to organize my room
+                or
+                closet. I was always a step ahead of her (what a good daughter, huh?) I’ve always been a
+                planner. I
+                would get back from school and do all my homework or projects right away. I hated procrastination.
+                It
+                would make me anxious and nervous if I knew I had something unfinished for school. I had to get it
+                done!</p>
+            <div className="text-center">
+                <img src="/img/about/ju-about-page-pic1.png"/>
+            </div>
 
             <p>Everyone has some skills that just come natural to them. It could be teaching, writing, being well
-                spoken,
-                being creative or being organized. However, I believe that even if you were not born with a specific
-                set of
-                skills you can always learn how to improve yourself and become more confident on it.
-                That happens with organizing. If you set your mind that you would like to live clutter free and you
-                are ready
-                to do something about it you can learn to be more organized. Trust me, anyone can learn to be more
-                organized.
-                I continuously learn ways on how to be more organized myself!</p>
-
-            <p>Being a planner was a skill that shaped me to be who I am today and helped me see that I LOVE
-                organizing.
-                I was 18 years old and living abroad in the US. Not realizing what was really happening, I would
-                just help
-                the ones around me with projects in their homes. After doing makeovers in their closets, garages,
-                basements,
-                people would tell me that I had great organization skills and that I should do that for a living.
-                But I never really listened. It wasn’t  until 2 years ago that I decided to pursue this career.
+                spoken, being creative or being organized. However, I believe that even if you were not born with a
+                specific set of skills you can always learn how to improve yourself and become more confident in it.
+                That happens with organizing. If you set your mind on living a clutter free life, and you
+                are
+                ready to do something about it, you can learn to be more organized. Trust me, anyone can learn to be
+                more
+                organized. I continuously learn ways on how to be more organized myself!
+                Being a planner was a skill that shaped me to be who I am today and helped me see that I LOVE
+                organizing. I was 18 years old and living abroad in the US. Not realizing what was really happening,
+                I
+                would help friends with projects in their homes. After doing makeovers in their closets, garages,
+                basements, people would tell me that I had great organization skills and that I should do that for a
+                living but I never really listened. It wasn’t until 2 years ago that I decided to pursue this
+                career.
                 Oh, and I’m so glad I did. The feeling of helping people to live a more clutter free life, and show
-                them that
-                they can enjoy their homes to its full potential, is the best reward.</p>
+                them
+                that they can enjoy their homes to its full potential, is the best reward. </p>
+            <div className="text-center">
+                <img src="/img/about/ju-about-page-pic2.jpg"/>
+            </div>
+            <p>I have been married to my wonderful husband, Juan, for 8 years now. We don’t have any kids yet but we do
+                have
+                2 furry little ones, Bailey and Sofi. They are our babies and we love them very much.
+                After living in San Diego for 5 years, we moved to Boston to stay closer to Juan's family. In 2015,
+                we bought a house in Stoneham, MA. We are still in the process of buying furniture and settling in.
+                It’s so exciting! Follow me on <a target="_blank" href="http://facebook.com/tackleclutter">social media</a> and
+                you will
+                see many projects and changes we have been making to our new house as we make it our
+                home.</p>
+            <div className="text-center">
+                <img src="/img/about/ju-about-page-pic3.jpg"/>
+                <img src="/img/about/ju-about-page-pic4.png"/>
+            </div>
+            <p>I've worked with many different clients and types of homes, big and small.
+                One of my goals as a professional
+                organizer is to show you that you don’t need to have a big fancy
+                house to have an organized home. It’s important to be realistic when creating an organization plan.
+                Be aware of the space you have, and
+                how much you are trying to fit in it. Decluttering first is a must if you want to create an
+                organized space. </p>
+            <p>Thank you for stopping by and I hope to <NavLink to="/contact">hear from you soon</NavLink>.</p>
+            <p>—Juliana</p>
         </div>
-        <div>
-            <span className="rounded-img ju-profile"><img src="img/ju-profile.jpg"/></span>
-        </div>
-    </Layout>
+    )
 }
