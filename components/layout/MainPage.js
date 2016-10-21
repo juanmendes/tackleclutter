c70c8e95db06fc1346abb32b2224dd257dfa7d46
Juan Mendes <jmendes@vmware.com>
2016-10-20 22:19:08 -0400

    Back to top button, part 2, should have been a single commit

--- components/layout/MainPage.js
+++ components/layout/MainPage.js
@@ -10,7 +10,13 @@ export default React.createClass({
                     <div className="row">{this.props.children}</div>
                 </div>
                 <Footer />
+                <button onClick={this.scrollToTop}
+                        className="back-to-top"><i className="fa fa-2x fa-chevron-up" aria-hidden="true"></i></button>
             </div>
         );
+    },
+
+    scrollToTop() {
+        document.body.scrollTop = innerWidth > 768 ? 101 : 0;
     }
 })
