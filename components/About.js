ffa2f40420c11566bad0bc713844de285de5aefd
Juan Mendes <jmendes@vmware.com>
2016-11-01 22:49:24 -0400

    About update, remove as a kid section

--- components/About.js
+++ components/About.js
@@ -7,26 +7,26 @@ export default function () {
 
             <h3>A little about me</h3>
 
-            <p>I’ve always been organized from a very young age. As a kid, I enjoyed organizing my dolls, my desk, my closet and making my room look nice. It was important to me. I can clearly remember myself organizing my pink wardrobe closet (actually my whole room was PINK!). It had three tall doors but I could only use two of them to keep my clothes and shoes, the other one my mom would keep our extra bed comforters. I would remove everything from my shelves and hangers and declutter by deciding what I would keep, pass along to my younger cousins or discard.</p>
+            <p>Being a planner was a skill that shaped me to be who I am today and helped me see that I LOVE organizing. I was 18 years old and living abroad in the US. Not realizing what was really happening, I would help friends with projects in their homes. After doing makeovers in their closets, garages, basements, people would tell me that I had great organization skills and that I should do that for a living but I never really listened. Finally, 2 years ago I decided to pursue this career. The feeling of helping people to live a more clutter free life, and show them that they can enjoy their homes to its full potential, is the best reward.</p>
 
             <div className="text-center">
                 <img src="/img/about/ju-about-page-pic1.png"/>
             </div>
 
-            <p>Everyone has some skills that just come natural to them. It could be teaching, writing, being well spoken, being creative or being organized. However, I believe that even if you were not born with a specific set of skills you can always learn how to improve yourself and become more confident in it. That happens with organizing. If you set your mind on living a clutter free life, and you are ready to do something about it, you can learn to be more organized. Trust me, anyone can learn to be more organized. I continuously learn ways on how to be more organized myself! Being a planner was a skill that shaped me to be who I am today and helped me see that I LOVE organizing. I was 18 years old and living abroad in the US. Not realizing what was really happening, I would help friends with projects in their homes. After doing makeovers in their closets, garages, basements, people would tell me that I had great organization skills and that I should do that for a living but I never really listened. Finally, 2 years ago I decided to pursue this career. The feeling of helping people to live a more clutter free life, and show them that they can enjoy their homes to its full potential, is the best reward.</p>
+            <p>Everyone has some skills that just come natural to them. It could be teaching, writing, being well spoken, being creative or being organized. However, I believe that even if you were not born with a specific set of skills you can always learn how to improve yourself and become more confident in it. That happens with organizing. If you set your mind on living a clutter free life, and you are ready to do something about it, you can learn to be more organized. Trust me, anyone can learn to be more organized. I continuously learn ways on how to be more organized myself! </p>
 
             <div className="text-center">
                 <img src="/img/about/ju-about-page-pic2.jpg"/>
             </div>
 
-            <p>I have been married to my wonderful husband, Juan, for 8 years now. We don’t have any kids yet but we do have 2 furry little ones, Bailey and Sofi.  After living in San Diego for 5 years, we moved to Boston to stay closer to Juan's family. In 2015, we bought a house in Stoneham, MA. We are still in the process of buying furniture and settling in. It’s so exciting! Follow me on social media and you will see many projects and changes we have been making to our new house as we make it our home.</p>
+            <p>I have been married to my wonderful husband, Juan, for 8 years now. We don’t have any kids yet but we do have 2 furry little ones, Bailey and Sofi.  After living in San Diego for 5 years, we moved to Boston to stay closer to Juan's family. In 2015, we bought a house in Stoneham, MA. We are still in the process of buying furniture and settling in. It’s so exciting! <a href="http://facebook.com/tackleclutter">Follow me on social media</a> and you will see many projects and changes we have been making to our new house as we make it our home.</p>
 
             <div className="text-center">
                 <img src="/img/about/ju-about-page-pic3.jpg"/>
                 <img src="/img/about/ju-about-page-pic4.png"/>
             </div>
             
-            <p>I've worked with many different clients and types of homes, big and small. One of my goals as a professional organizer is to show you that no matter what kind of house or space you have, you can get organized and live clutter free. I will help you address your space and create an organization plan. It’s important to be aware of the space you have, and how much you are trying to fit in it. Decluttering first is a must if you want to create an organized space.</p>
+            <p>I've worked with many different clients and types of homes, big and small. One of my goals as a professional organizer is to show you that no matter what kind of house or space you have, you can get organized and live clutter free. I will help you address your space and create a customized organization plan that works for you. It’s important to be aware of the space you have, and how much you are trying to fit in it. Decluttering first is a must if you want to create an organized space.</p>
 
             <p>Thank you for stopping by and I hope to <NavLink to="/contact">hear from you soon</NavLink>.</p>
 
